9433d588ce1de2479b469e5d61ac72e0f2073054
ccqstark <1367305698@qq.com>
2021-06-20 16:38:24 +0800

    多环境配置

--- ioc02/src/main/java/com/ccqstark/ioc/JavaConfig.java
+++ ioc02/src/main/java/com/ccqstark/ioc/JavaConfig.java
@@ -3,6 +3,7 @@ package com.ccqstark.ioc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class JavaConfig {
@@ -10,14 +11,36 @@ public class JavaConfig {
     // 条件注解，根据条件不同选择不同的实现或bean，用@Conditional指定不同的条件类
     @Bean("cmd")
     @Conditional(WindowCondition.class)
-    ShowCmd winCmd(){
+    ShowCmd winCmd() {
         return new WindowShowCmd();
     }
 
     @Bean("cmd")
     @Conditional(MacCondition.class)
-    ShowCmd macCmd(){
+    ShowCmd macCmd() {
         return new MacShowCmd();
+    }
+
+
+    // 多环境切换用@Profile配置环境信息
+    @Bean
+    @Profile("dev")
+    DataSource devDs() {
+        DataSource ds = new DataSource();
+        ds.setUrl("jdbc:mysql://dev");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        return ds;
+    }
+
+    @Bean
+    @Profile("prod")
+    DataSource prodDs() {
+        DataSource ds = new DataSource();
+        ds.setUrl("jdbc:mysql://prod");
+        ds.setUsername("crud");
+        ds.setPassword("crud");
+        return ds;
     }
 
 }
--- ioc02/src/main/java/com/ccqstark/ioc/Main.java
+++ ioc02/src/main/java/com/ccqstark/ioc/Main.java
@@ -1,13 +1,42 @@
 package com.ccqstark.ioc;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
+
+
+
+    }
+
+    // xml方式实现多环境配置
+    private static void multiplyEnvironmentConfigByXML() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
+        ctx.getEnvironment().setActiveProfiles("dev");
+        ctx.setConfigLocation("applicationContext.xml");
+        ctx.refresh();
+        DataSource ds = ctx.getBean(DataSource.class);
+        System.out.println(ds);
+    }
+
+    // 注解方式实现多环境配置
+    private static void multiplyEnvironmentConfigByAnnotation() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.getEnvironment().setActiveProfiles("prod");
+        ctx.register(JavaConfig.class);
+        ctx.refresh();
+        DataSource ds = ctx.getBean(DataSource.class);
+        System.out.println(ds);
+    }
+
+
+    // 条件注解测试方法
+    private static void conditionAnnotationTest() {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
-        ShowCmd cmd = (ShowCmd)ctx.getBean("cmd");
-        String s  = cmd.showCmd();
+        ShowCmd cmd = (ShowCmd) ctx.getBean("cmd");
+        String s = cmd.showCmd();
         System.out.println("s = " + s);
     }
 
